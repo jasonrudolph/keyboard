018e5e8cde888e05924c2802a814b1a43f64caf2
Jason Rudolph <jason@jasonrudolph.com>
2016-11-19 20:07:07 -0500

    Translate caps lock + space to control + space

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -4,7 +4,7 @@ betterCapsLockMode = hs.hotkey.modal.new({}, 'F20')
 -- When caps lock is pressed in combination with another key, translate it to
 -- control plus that other key. For example, translate "caps lock + e" to
 -- "control + e" to go to the end of the line.
-for _, key in ipairs({ 'a', 'b', 'd', 'e', 'f', 'h', 'l', 'n', 'o', 'p', 't' }) do
+for _, key in ipairs({ 'space', 'a', 'b', 'd', 'e', 'f', 'h', 'l', 'n', 'o', 'p', 't' }) do
   betterCapsLockMode:bind({}, key, nil, function()
     hs.eventtap.keyStroke({'⌃'}, key)
     betterCapsLockMode.triggered = true
