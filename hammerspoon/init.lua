d4ec969f1042bb6978f21c6ba91167e024f47a3d
Jason Rudolph <jason@jasonrudolph.com>
2016-11-19 20:52:49 -0500

    Port hyper key app launching to macOS Sierra

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,3 +1,7 @@
+--------------------------------------------------------------------------------
+-- BetterCapsLock Mode
+--------------------------------------------------------------------------------
+
 -- A global variable for BetterCapsLock Mode
 betterCapsLockMode = hs.hotkey.modal.new({}, 'F20')
 
@@ -28,3 +32,41 @@ end
 
 -- Bind the BetterCapsLock key
 hs.hotkey.bind({}, 'F19', pressedF19, releasedF19)
+
+--------------------------------------------------------------------------------
+-- Hyper Mode
+--------------------------------------------------------------------------------
+
+-- A global variable for Hyper Mode
+hyperMode = hs.hotkey.modal.new({}, 'F18')
+
+-- Keybindings for launching apps in Hyper Mode
+hyperModeAppMappings = {
+  { 'a', 'iTunes' },            -- "A" for "Apple Music"
+  { 'b', 'Google Chrome' },     -- "B" for "Browser"
+  { 'c', 'Slack' },             -- "C for "Chat"
+  { 'd', 'Remember The Milk' }, -- "D" for "Do!" ... or "Done!"
+  { 'e', 'Atom Beta' },         -- "E" for "Editor"
+  { 'f', 'Finder' },            -- "F" for "Finder"
+  { 'g', 'Mailplane 3' },       -- "G" for "Gmail"
+  { 't', 'iTerm' },             -- "T" for "Terminal"
+}
+
+for i, mapping in ipairs(hyperModeAppMappings) do
+  hyperMode:bind({}, mapping[1], function()
+    hs.application.launchOrFocus(mapping[2])
+  end)
+end
+
+-- Enter Hyper Mode when F17 (right option key) is pressed
+pressedF17 = function()
+  hyperMode:enter()
+end
+
+-- Leave Hyper Mode when F17 (right option key) is released.
+releasedF17 = function()
+  hyperMode:exit()
+end
+
+-- Bind the Hyper key
+hs.hotkey.bind({}, 'F17', pressedF17, releasedF17)
