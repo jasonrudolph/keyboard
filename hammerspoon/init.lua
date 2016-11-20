13bdcee70b30786c0be2420b8241b81abc5c6ae8
Jason Rudolph <jason@jasonrudolph.com>
2016-11-19 19:54:19 -0500

    Port core caps lock mappings for macOS Sierra
    Karabiner doesn't currently run on macOS Sierra. Until it does, perhaps
    we can use Karabiner-Elements and Hammerspoon to get the functionality
    that we used to get from Karabiner. For starters, let's configure caps
    lock so that we can:
    
    - Tap caps lock for escape
    - Press caps lock plus and another key to trigger *control* plus that
      other key (e.g., "caps lock + e" to trigger "control + e" to move to
      the end of the line)
    
    Depends on:
    
    - Hammperspoon (http://www.hammerspoon.org)
    - Karabiner-Elements (https://github.com/tekezo/Karabiner-Elements)

---
+++ hammerspoon/init.lua
@@ -0,0 +1,30 @@
+-- A global variable for BetterCapsLock Mode
+betterCapsLockMode = hs.hotkey.modal.new({}, 'F20')
+
+-- When caps lock is pressed in combination with another key, translate it to
+-- control plus that other key. For example, translate "caps lock + e" to
+-- "control + e" to go to the end of the line.
+for _, key in ipairs({ 'a', 'b', 'd', 'e', 'f', 'h', 'l', 'n', 'o', 'p', 't' }) do
+  betterCapsLockMode:bind({}, key, nil, function()
+    hs.eventtap.keyStroke({'⌃'}, key)
+    betterCapsLockMode.triggered = true
+  end)
+end
+
+-- Enter BetterCapsLock Mode when F19 (caps lock) is pressed
+pressedF19 = function()
+  betterCapsLockMode.triggered = false
+  betterCapsLockMode:enter()
+end
+
+-- Leave BetterCapsLock Mode when F19 (caps lock) is pressed.
+--   Send ESCAPE if no other keys are pressed.
+releasedF19 = function()
+  betterCapsLockMode:exit()
+  if not betterCapsLockMode.triggered then
+    hs.eventtap.keyStroke({}, 'ESCAPE')
+  end
+end
+
+-- Bind the BetterCapsLock key
+hs.hotkey.bind({}, 'F19', pressedF19, releasedF19)
