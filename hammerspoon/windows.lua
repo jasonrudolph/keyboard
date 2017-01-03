1f5dd532ebcfea5f5eb4a44b1a12ee1ded419b7e
Jason Rudolph <jason@jasonrudolph.com>
2017-01-02 19:48:33 -0500

    Remove obsolete reference to 'BetterCapsLock'

--- hammerspoon/windows.lua
+++ hammerspoon/windows.lua
@@ -171,10 +171,10 @@ end
 -- Define WindowLayout Mode
 --
 -- WindowLayout Mode allows you to manage window layout using keyboard shortcuts
--- that are on the home row, or very close to it. Use BetterCapsLock+w to turn
+-- that are on the home row, or very close to it. Use Control+w to turn
 -- on WindowLayout mode. Then, use any shortcut below to perform a window layout
 -- action. For example, to send the window left, press and release
--- BetterCapsLock+w, and then press h.
+-- Control+w, and then press h.
 --
 --   h/j/k/l => send window to the left/bottom/top/right half of the screen
 --   i => send window to the upper left quarter of the screen
