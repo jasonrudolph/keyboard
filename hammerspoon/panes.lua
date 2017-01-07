9559c17d0df3a806b1e37ac2f0b078bdff77457a
Jason Rudolph <jason@jasonrudolph.com>
2017-01-07 17:31:34 -0500

    Refactor panes.lua

--- hammerspoon/panes.lua
+++ hammerspoon/panes.lua
@@ -1,45 +1,57 @@
 local wf = hs.window.filter
 
--- Use control + dash to split panes horizontally in iTerm2
-local splitHorizontalHotkey = hs.hotkey.new({'ctrl'}, '-', function()
-  keyUpDown({'cmd', 'shift'}, 'd')
-end)
+local itermHotkeyMappings = {
+  -- Use control + dash to split panes horizontally
+  {
+    from = {{'ctrl'}, '-'},
+    to   = {{'cmd', 'shift'}, 'd'}
+  },
 
--- Use control + pipe to split panes vertically in iTerm2
-local splitVerticalHotkey = hs.hotkey.new({'ctrl', 'shift'}, '\\', function()
-  keyUpDown({'cmd'}, 'd')
-end)
+  -- Use control + pipe to split panes vertically
+  {
+    from = {{'ctrl', 'shift'}, '\\'},
+    to   = {{'cmd'}, 'd'}
+  },
 
--- Use control + h/j/k/l to move left/down/up/right by one pane in iTerm2
-local paneDownHotkey = hs.hotkey.new({'ctrl'}, 'j', function()
-  keyUpDown({'cmd', 'alt'}, 'down')
-end)
-local paneUpHotkey = hs.hotkey.new({'ctrl'}, 'k', function()
-  keyUpDown({'cmd', 'alt'}, 'up')
-end)
-local paneLeftHotkey = hs.hotkey.new({'ctrl'}, 'h', function()
-  keyUpDown({'cmd', 'alt'}, 'left')
-end)
-local paneRightHotkey = hs.hotkey.new({'ctrl'}, 'l', function()
-  keyUpDown({'cmd', 'alt'}, 'right')
+  -- Use control + h/j/k/l to move left/down/up/right by one pane in iTerm2
+  {
+    from = {{'ctrl'}, 'h'},
+    to   = {{'cmd', 'alt'}, 'left'}
+  },
+  {
+    from = {{'ctrl'}, 'j'},
+    to   = {{'cmd', 'alt'}, 'down'}
+  },
+  {
+    from = {{'ctrl'}, 'k'},
+    to   = {{'cmd', 'alt'}, 'up'}
+  },
+  {
+    from = {{'ctrl'}, 'l'},
+    to   = {{'cmd', 'alt'}, 'right'}
+  },
+}
+
+local itermHotkeys = hs.fnutils.imap(itermHotkeyMappings, function(mapping)
+  local fromMods = mapping['from'][1]
+  local fromKey = mapping['from'][2]
+  local toMods = mapping['to'][1]
+  local toKey = mapping['to'][2]
+  return hs.hotkey.new(fromMods, fromKey, function()
+    keyUpDown(toMods, toKey)
+  end)
 end)
 
 local terminalWindowFilter = wf.new('iTerm2')
 
 terminalWindowFilter:subscribe(wf.windowFocused, function()
-  splitHorizontalHotkey:enable()
-  splitVerticalHotkey:enable()
-  paneUpHotkey:enable()
-  paneDownHotkey:enable()
-  paneLeftHotkey:enable()
-  paneRightHotkey:enable()
+  hs.fnutils.each(itermHotkeys, function(hotkey)
+    hotkey:enable()
+  end)
 end)
 
 terminalWindowFilter:subscribe(wf.windowUnfocused, function()
-  splitHorizontalHotkey:disable()
-  splitVerticalHotkey:disable()
-  paneUpHotkey:disable()
-  paneDownHotkey:disable()
-  paneLeftHotkey:disable()
-  paneRightHotkey:disable()
+  hs.fnutils.each(itermHotkeys, function(hotkey)
+    hotkey:disable()
+  end)
 end)
