0f65ca42d29a15be5715386c996b0f83ce1ecafa
Jason Rudolph <jason@jasonrudolph.com>
2017-01-10 09:02:54 -0500

    Extract fn: enableHotkeyForWindowsMatchingFilter

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -6,6 +6,25 @@ keyUpDown = function(modifiers, key)
   hs.eventtap.event.newKeyEvent(modifiers, key, false):post()
 end
 
+-- Subscribe to the necessary events on the given window filter such that the
+-- given hotkey is enabled for windows that match the window filter and disabled
+-- for windows that don't match the window filter.
+--
+-- windowFilter - An hs.window.filter object describing the windows for which
+--                the hotkey should be enabled.
+-- hotkey       - The hs.hotkey object to enable/disable.
+--
+-- Returns nothing.
+enableHotkeyForWindowsMatchingFilter = function(windowFilter, hotkey)
+  windowFilter:subscribe(hs.window.filter.windowFocused, function()
+    hotkey:enable()
+  end)
+
+  windowFilter:subscribe(hs.window.filter.windowUnfocused, function()
+    hotkey:disable()
+  end)
+end
+
 require('control-escape')
 require('delete-words')
 require('hyper')
--- hammerspoon/panes.lua
+++ hammerspoon/panes.lua
@@ -1,5 +1,3 @@
-local wf = hs.window.filter
-
 local itermHotkeyMappings = {
   -- Use control + dash to split panes horizontally
   {
@@ -32,26 +30,14 @@ local itermHotkeyMappings = {
   },
 }
 
-local itermHotkeys = hs.fnutils.imap(itermHotkeyMappings, function(mapping)
+local terminalWindowFilter = hs.window.filter.new('iTerm2')
+local itermHotkeys = hs.fnutils.each(itermHotkeyMappings, function(mapping)
   local fromMods = mapping['from'][1]
   local fromKey = mapping['from'][2]
   local toMods = mapping['to'][1]
   local toKey = mapping['to'][2]
-  return hs.hotkey.new(fromMods, fromKey, function()
+  local hotkey = hs.hotkey.new(fromMods, fromKey, function()
     keyUpDown(toMods, toKey)
   end)
-end)
-
-local terminalWindowFilter = wf.new('iTerm2')
-
-terminalWindowFilter:subscribe(wf.windowFocused, function()
-  hs.fnutils.each(itermHotkeys, function(hotkey)
-    hotkey:enable()
-  end)
-end)
-
-terminalWindowFilter:subscribe(wf.windowUnfocused, function()
-  hs.fnutils.each(itermHotkeys, function(hotkey)
-    hotkey:disable()
-  end)
+  enableHotkeyForWindowsMatchingFilter(terminalWindowFilter, hotkey)
 end)
