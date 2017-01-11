cee11c7d2bb2b57449bbdd62abf261a988b04d6a
Jason Rudolph <jason@jasonrudolph.com>
2017-01-11 11:38:52 -0500

    Define keybinding earlier
    When working on these configs, if there's an error in the code, it
    will prevent the subsequent code from getting executed. In those cases,
    we need to fix the issue and reload the config. To make it easier to
    reload the config, it's helpful to bind the hotkey for reloading the
    config *before* any of the other config code.

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,5 +1,10 @@
 local log = hs.logger.new('init.lua', 'debug')
 
+-- Use Control+` to reload Hammerspoon config
+hs.hotkey.bind({'ctrl'}, '`', nil, function()
+  hs.reload()
+end)
+
 keyUpDown = function(modifiers, key)
   log.d('Sending keystroke:', hs.inspect(modifiers), key)
   hs.eventtap.event.newKeyEvent(modifiers, key, true):post()
@@ -32,10 +37,5 @@ require('markdown')
 require('panes')
 require('super')
 require('windows')
-
--- Use Control+` to reload Hammerspoon config
-hs.hotkey.bind({'ctrl'}, '`', nil, function()
-  hs.reload()
-end)
 
 hs.notify.new({title='Hammerspoon', informativeText='Ready to rock 🤘'}):send()
