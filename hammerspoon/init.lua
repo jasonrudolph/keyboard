d4bc97dcd1c34b1f35580eaa8ff83bbd75bf115b
Jason Rudolph <jason@jasonrudolph.com>
2017-06-11 20:16:00 -0400

    🎨 Put a little whitespace to work

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -8,6 +8,7 @@ end)
 keyUpDown = function(modifiers, key)
   -- Un-comment & reload config to log each keystroke that we're triggering
   -- log.d('Sending keystroke:', hs.inspect(modifiers), key)
+
   hs.eventtap.keyStroke(modifiers, key, 0)
 end
 
