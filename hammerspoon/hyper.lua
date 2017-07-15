934c1c62a5abe7b4bf9b0eb26bf8f74e03d370e1
Jason Rudolph <jason@jasonrudolph.com>
2017-07-14 21:10:17 -0400

    Use Karabiner-Elements to provide Hyper key
    Now that Karabiner-Elements supports "complex modifications," we can use
    Karabiner-Elements to map right option to command+control+option+shift
    just like we used to do in the good ol' days of Karabiner:
    
    https://github.com/jasonrudolph/keyboard/blob/e19948b/karabiner/private.xml#L99-L107
    
    And with that mapping in place, we can vastly simplify hyper.lua 😅
    *and* we can likely avoid the occasional issues that we've been seeing
    with exiting Hyper mode:
    
      https://github.com/jasonrudolph/keyboard/pull/27#issue-242557761

--- hammerspoon/hyper.lua
+++ hammerspoon/hyper.lua
@@ -1,17 +1,3 @@
--- A global variable for Hyper Mode
-hyperMode = hs.hotkey.modal.new({}, 'F18')
-
-local message = require('keyboard.status-message')
-statusMessage = message.new('Hyper Mode')
-
-function hyperMode:entered()
-  statusMessage:show()
-end
-
-function hyperMode:exited()
-  statusMessage:hide()
-end
-
 -- Keybindings for launching apps in Hyper Mode
 hyperModeAppMappings = {
   { 'a', 'iTunes' },                -- "A" for "Apple Music"
@@ -26,33 +12,7 @@ hyperModeAppMappings = {
 }
 
 for i, mapping in ipairs(hyperModeAppMappings) do
-  hyperMode:bind({}, mapping[1], function()
+  hs.hotkey.bind({'shift', 'ctrl', 'alt', 'cmd'}, mapping[1], function()
     hs.application.launchOrFocus(mapping[2])
   end)
 end
-
--- Enter Hyper Mode when F17 (right option key) is pressed
-pressedF17 = function()
-  hyperMode:enter()
-
-  -- The releasedF17 function *should* get called when the Hyper key is
-  -- released, but for some reason it seems that Hammerspoon sometimes fails to
-  -- invoke that function. If Hammerspoon fails to invoke the releasedF17
-  -- function, we end up stuck in Hyper Mode until the user once again presses
-  -- and releases the Hyper Key. 🙀
-  --
-  -- To reduce the likelihood of getting stuck in Hyper Mode, set a timer to
-  -- automatically exit Hyper Mode after a short while.
-  local TIMEOUT_IN_SECONDS = 10
-  hs.timer.doAfter(TIMEOUT_IN_SECONDS, function()
-    hyperMode:exit()
-  end)
-end
-
--- Leave Hyper Mode when F17 (right option key) is released.
-releasedF17 = function()
-  hyperMode:exit()
-end
-
--- Bind the Hyper key
-f17 = hs.hotkey.bind({}, 'F17', pressedF17, releasedF17)
