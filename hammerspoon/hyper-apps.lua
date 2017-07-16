2004b9bec506e0478ff917c063abbce5621f9747
Jason Rudolph <jason@jasonrudolph.com>
2017-07-16 17:00:26 -0400

    Extract Hyper Mode keybindings into data file
    By gitignoring this file, we can ship with a default set of keybindings,
    and users can customize the file as they see fit, without having to
    reapply those customizations each time they pull down the latest code
    for the repo.

---
+++ hammerspoon/hyper-apps.lua
@@ -0,0 +1,14 @@
+-- Keybindings for launching apps in Hyper Mode
+--
+-- Edit this file to configure shortcuts to launch your most commonly-used apps.
+return {
+  { 'a', 'iTunes' },                -- "A" for "Apple Music"
+  { 'b', 'Google Chrome' },         -- "B" for "Browser"
+  { 'c', 'Hackable Slack Client' }, -- "C for "Chat"
+  { 'd', 'Remember The Milk' },     -- "D" for "Do!" ... or "Done!"
+  { 'e', 'Atom' },                  -- "E" for "Editor"
+  { 'f', 'Finder' },                -- "F" for "Finder"
+  { 'g', 'Mailplane 3' },           -- "G" for "Gmail"
+  { 's', 'Slack' },                 -- "S" for "Slack"
+  { 't', 'iTerm' },                 -- "T" for "Terminal"
+}
--- hammerspoon/hyper.lua
+++ hammerspoon/hyper.lua
@@ -1,16 +1,5 @@
 -- Keybindings for launching apps in Hyper Mode
-hyperModeAppMappings = {
-  { 'a', 'iTunes' },                -- "A" for "Apple Music"
-  { 'b', 'Google Chrome' },         -- "B" for "Browser"
-  { 'c', 'Hackable Slack Client' }, -- "C for "Chat"
-  { 'd', 'Remember The Milk' },     -- "D" for "Do!" ... or "Done!"
-  { 'e', 'Atom' },                  -- "E" for "Editor"
-  { 'f', 'Finder' },                -- "F" for "Finder"
-  { 'g', 'Mailplane 3' },           -- "G" for "Gmail"
-  { 's', 'Slack' },                 -- "S" for "Slack"
-  { 't', 'iTerm' },                 -- "T" for "Terminal"
-}
-
+hyperModeAppMappings = require('keyboard.hyper-apps')
 for i, mapping in ipairs(hyperModeAppMappings) do
   hs.hotkey.bind({'shift', 'ctrl', 'alt', 'cmd'}, mapping[1], function()
     hs.application.launchOrFocus(mapping[2])
