587d53825344349b6ece982153c9d49b48cb6087
Jason Rudolph <jason@jasonrudolph.com>
2017-08-03 20:14:53 -0400

    :art:

--- hammerspoon/hyper-apps-defaults.lua
+++ hammerspoon/hyper-apps-defaults.lua
@@ -4,13 +4,13 @@
 -- this file, save it as `hyper-apps.lua`, and edit the table below to configure
 -- your preferred shortcuts.
 return {
-  { 'a', 'iTunes' },                -- "A" for "Apple Music"
-  { 'b', 'Google Chrome' },         -- "B" for "Browser"
-  { 'c', 'Slack' },                 -- "C for "Chat"
-  { 'd', 'Remember The Milk' },     -- "D" for "Do!" ... or "Done!"
-  { 'e', 'Atom' },                  -- "E" for "Editor"
-  { 'f', 'Finder' },                -- "F" for "Finder"
-  { 'g', 'Mailplane 3' },           -- "G" for "Gmail"
-  { 's', 'Slack' },                 -- "S" for "Slack"
-  { 't', 'iTerm' },                 -- "T" for "Terminal"
+  { 'a', 'iTunes' },            -- "A" for "Apple Music"
+  { 'b', 'Google Chrome' },     -- "B" for "Browser"
+  { 'c', 'Slack' },             -- "C for "Chat"
+  { 'd', 'Remember The Milk' }, -- "D" for "Do!" ... or "Done!"
+  { 'e', 'Atom' },              -- "E" for "Editor"
+  { 'f', 'Finder' },            -- "F" for "Finder"
+  { 'g', 'Mailplane 3' },       -- "G" for "Gmail"
+  { 's', 'Slack' },             -- "S" for "Slack"
+  { 't', 'iTerm' },             -- "T" for "Terminal"
 }
