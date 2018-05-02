b6e7d24ff0366275873d5c918160147c09f00231
Jason Rudolph <jason@jasonrudolph.com>
2018-05-01 20:58:16 -0400

    Implement Hyper Mode exclusively with Hammerspoon
    - Hyper Mode is now activated by simultaneously pressing the 'k' and 'l'
      keys and holding them down. This is similar to activating (S)uper
      (D)uper Mode by simultaneously pressing the 's' and 'd' keys and
      holding them down.
    - Hyper Mode is no longer activated by pressing the right option key.
    - The format of the Hyper Mode key-to-app mappings has changed.
      See hammerspoon/hyper-apps-defaults.lua.

--- hammerspoon/hyper.lua
+++ hammerspoon/hyper.lua
@@ -1,13 +1,30 @@
-local status, hyperModeAppMappings = pcall(require, 'keyboard.hyper-apps')
+local eventtap = hs.eventtap
+local eventTypes = hs.eventtap.event.types
+local simultaneousKeypressModal = require('keyboard.simultaneous-keypress-modal')
 
+-- Look for custom Hyper Mode app mappings. If there are none, then use the
+-- default mappings.
+local status, hyperModeAppMappings = pcall(require, 'keyboard.hyper-apps')
 if not status then
   hyperModeAppMappings = require('keyboard.hyper-apps-defaults')
 end
 
-for i, mapping in ipairs(hyperModeAppMappings) do
-  local key = mapping[1]
-  local app = mapping[2]
-  hs.hotkey.bind({'shift', 'ctrl', 'alt', 'cmd'}, key, function()
+-- Create a hotkey that will enter Hyper Mode when 'k' and 'l' are pressed
+-- simultaneously.
+hyperMode = simultaneousKeypressModal.new('Hyper Mode', 'k', 'l')
+
+--------------------------------------------------------------------------------
+-- Watch for key-down events in Hyper Mode, and trigger app associated with the
+-- given key.
+--------------------------------------------------------------------------------
+hyperModeKeyListener = eventtap.new({ eventTypes.keyDown }, function(event)
+  if not hyperMode:isActive() then
+    return false
+  end
+
+  local app = hyperModeAppMappings[event:getCharacters(true):lower()]
+
+  if app then
     if (type(app) == 'string') then
       hs.application.open(app)
     elseif (type(app) == 'function') then
@@ -15,5 +32,9 @@ for i, mapping in ipairs(hyperModeAppMappings) do
     else
       hs.logger.new('hyper'):e('Invalid mapping for Hyper +', key)
     end
-  end)
-end
+    return true
+  end
+end):start()
+
+--- We're all set. Now we just enable Hyper Mode and get to work. 👔
+hyperMode:enable()
