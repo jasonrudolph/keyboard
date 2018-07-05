2dc3490fcd5c3530605125ad317c74ffb6da3cb4
Jason Rudolph <jason@jasonrudolph.com>
2018-07-05 10:51:39 -0400

    Revert "Merge pull request #38"
    This reverts commit bd2f8ff879010f454b7ed642015eccae2767c90d, reversing
    changes made to 3601e5ebfab192249a059a74ec8d5d5864694113.

--- hammerspoon/hyper.lua
+++ hammerspoon/hyper.lua
@@ -1,20 +1,13 @@
--- Look for custom Hyper Mode app mappings. If there are none, then use the
--- default mappings.
 local status, hyperModeAppMappings = pcall(require, 'keyboard.hyper-apps')
+
 if not status then
   hyperModeAppMappings = require('keyboard.hyper-apps-defaults')
 end
 
--- Create a hotkey that will enter Hyper Mode when 'alt' is tapped (i.e.,
--- when 'alt' is pressed and then released in quick succession).
-local hotkey = require('keyboard.tap-modifier-for-hotkey')
-hyperMode = hotkey.new('alt')
-
--- Bind the hotkeys that will be active when we're in Hyper Mode
 for i, mapping in ipairs(hyperModeAppMappings) do
   local key = mapping[1]
   local app = mapping[2]
-  hyperMode:bind(key, function()
+  hs.hotkey.bind({'shift', 'ctrl', 'alt', 'cmd'}, key, function()
     if (type(app) == 'string') then
       hs.application.open(app)
     elseif (type(app) == 'function') then
@@ -24,16 +17,3 @@ for i, mapping in ipairs(hyperModeAppMappings) do
     end
   end)
 end
-
--- Show a status message when we're in Hyper Mode
-local message = require('keyboard.status-message')
-hyperMode.statusMessage = message.new('Hyper Mode')
-hyperMode.entered = function()
-  hyperMode.statusMessage:show()
-end
-hyperMode.exited = function()
-  hyperMode.statusMessage:hide()
-end
-
--- We're all set. Now we just enable Hyper Mode and get to work. 👔
-hyperMode:enable()
