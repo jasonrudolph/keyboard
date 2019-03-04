38cb1fee5d32c5bf56b894bf0fb18f159634d9a7
denosawr <denosawr@vivaldi.net>
2019-03-04 12:09:40 +0800

    Fix bug when typing "s" and "d" + any character
    Before this, typing "s" or "d" along with any other character in quick succession (< MAX_TIME_BETWEEN_SIMULTANEOUS_KEY_PRESSES) would end up inserting that character before the "s" or "d". This commit fixes that.

--- hammerspoon/super.lua
+++ hammerspoon/super.lua
@@ -9,6 +9,8 @@ local MAX_TIME_BETWEEN_SIMULTANEOUS_KEY_PRESSES = 0.04 -- 40 milliseconds
 
 local superDuperMode = {
   statusMessage = message.new('(S)uper (D)uper Mode'),
+  nextKeyAfter = -1,
+
   enter = function(self)
     if not self.active then self.statusMessage:show() end
     self.active = true
@@ -32,7 +34,12 @@ superDuperModeActivationListener = eventtap.new({ eventTypes.keyDown }, function
     return false
   end
 
+  if superDuperMode.active then
+    return true
+  end
+
   local characters = event:getCharacters()
+  local keycode = event:getKeyCode()
 
   if characters == 's' then
     if superDuperMode.ignoreNextS then
@@ -51,6 +58,10 @@ superDuperModeActivationListener = eventtap.new({ eventTypes.keyDown }, function
       else
         superDuperMode.ignoreNextS = true
         keyUpDown({}, 's')
+        if (superDuperMode.nextKeyAfter ~= -1) then
+          hs.eventtap.keyStroke({}, hs.keycodes.map[superDuperMode.nextKeyAfter], 0)
+          superDuperMode.nextKeyAfter = -1
+        end
         return false
       end
     end)
@@ -72,10 +83,20 @@ superDuperModeActivationListener = eventtap.new({ eventTypes.keyDown }, function
       else
         superDuperMode.ignoreNextD = true
         keyUpDown({}, 'd')
+        if (superDuperMode.nextKeyAfter ~= -1) then
+          hs.eventtap.keyStroke({}, hs.keycodes.map[superDuperMode.nextKeyAfter], 0)
+          superDuperMode.nextKeyAfter = -1
+        end
         return false
       end
     end)
     return true
+  else
+    if superDuperMode.isSDown or superDuperMode.isDDown then
+      -- We use keycodes, as that allows us to send return, space, delete, etc.
+      superDuperMode.nextKeyAfter = keycode
+      return true
+    end
   end
 end):start()
 
