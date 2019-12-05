b1ed8b09f40e96491489f8e9d6b7811ee3505ef2
Pierre Spring <pierre.spring@caillou.ch>
2019-12-05 13:35:13 +0100

    Cycle app window if frontmost

--- hammerspoon/hyper.lua
+++ hammerspoon/hyper.lua
@@ -4,12 +4,22 @@ if not status then
   hyperModeAppMappings = require('keyboard.hyper-apps-defaults')
 end
 
+local switchToApp = function (app)
+  local frontmostApp = hs.application.frontmostApplication()
+  local openApp = hs.application.get(app)
+  if openApp and frontmostApp:name() == openApp:name() then
+    hs.eventtap.keyStroke({'command'}, '`', 0)
+  else
+    hs.application.open(app)
+  end
+end
+
 for i, mapping in ipairs(hyperModeAppMappings) do
   local key = mapping[1]
   local app = mapping[2]
   hs.hotkey.bind({'shift', 'ctrl', 'alt', 'cmd'}, key, function()
     if (type(app) == 'string') then
-      hs.application.open(app)
+      switchToApp(app)
     elseif (type(app) == 'function') then
       app()
     else
